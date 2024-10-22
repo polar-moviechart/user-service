2ed01e220bed1f289c992deb7b29ddbb3f8c74b7
stk346 <stk346@gmail.com>
2024-10-23 03:49:54 +0900

    feat: UserController.kakaoLogin() - 카카로 로그인 기능 구현

---
+++ src/main/java/com/polar_moviechart/userservice/domain/entity/Role.java
@@ -0,0 +1,6 @@
+package com.polar_moviechart.userservice.domain.entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/polar_moviechart/userservice/domain/service/UserQueryService.java
@@ -0,0 +1,19 @@
+package com.polar_moviechart.userservice.domain.service;
+
+import com.polar_moviechart.userservice.domain.entity.AuthType;
+import com.polar_moviechart.userservice.domain.entity.User;
+import com.polar_moviechart.userservice.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryService {
+    private final UserRepository userRepository;
+
+    public Optional<User> getUser(AuthType authType, Long userId) {
+        return userRepository.findByAuthTypeAndUserId(authType, userId);
+    }
+}
