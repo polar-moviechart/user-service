954e8615507846f73058f17b1a61557aacba2a8a
stk346 <stk346@gmail.com>
2024-11-07 13:27:03 +0900

    refactor: CustomResponse에서 ErrorInfo를 파라미터로 받는 생성자 추가 및 setter 제거

--- src/main/java/com/polar_moviechart/userservice/exception/ErrorInfo.java
+++ src/main/java/com/polar_moviechart/userservice/exception/ErrorInfo.java
@@ -1,25 +1,23 @@
 package com.polar_moviechart.userservice.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-public enum ErrorCode {
+public enum ErrorInfo {
 
     DEFAULT_ERROR("9999", "예기치 못한 오류가 발생했습니다." + "\n" + "불편을 드려 죄송합니다."),
     TOKEN_CREATION_ERROR("100", "로그인 중 문제가 발생했습니다."),
     NOT_ALLOWED("104", "접근 권한이 없습니다.");
 
-    private final String code;
+    private String code = "U101";
     private final String message;
 
-    ErrorCode(String code, String message) {
+    ErrorInfo(String code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    ErrorCode(String message) {
-        this.code = "1000";
+    ErrorInfo(String message) {
         this.message = message;
     }
 }
