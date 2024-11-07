d869d8adc4d854e29cb3029cb6d684acfbebff62
stk346 <stk346@gmail.com>
2024-11-07 15:18:53 +0900

    refactor: application.yml - api 라우팅 변경

--- src/main/java/com/polar_moviechart/userservice/domain/controller/internalapi/UserControllerInternal.java
+++ src/main/java/com/polar_moviechart/userservice/domain/controller/internalapi/UserControllerInternal.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api-internal/v1/users")
+@RequestMapping("/internal/api/v1/users")
 public class UserControllerInternal {
 
     private final UserQueryService userQueryService;
