1afb4e3acae281db27be1bb6832c288b839dee55
stk346 <stk346@gmail.com>
2024-11-11 06:52:52 +0900

    refactor: KakaoLoginReq - request명 수정

--- src/main/java/com/polar_moviechart/userservice/domain/controller/publicapi/UserControllerPublic.java
+++ src/main/java/com/polar_moviechart/userservice/domain/controller/publicapi/UserControllerPublic.java
@@ -3,16 +3,18 @@ package com.polar_moviechart.userservice.domain.controller.publicapi;
 import com.polar_moviechart.userservice.domain.entity.Role;
 import com.polar_moviechart.userservice.domain.service.jwt.JwtProvider;
 import com.polar_moviechart.userservice.domain.service.jwt.TokenResponse;
-import com.polar_moviechart.userservice.domain.service.kakao.KakaoUserInfoDto;
+import com.polar_moviechart.userservice.domain.service.kakao.KakaoLoginReq;
 import com.polar_moviechart.userservice.domain.service.kakao.KakaoUserProcessor;
 import com.polar_moviechart.userservice.utils.CustomResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/public/api/v1/users")
@@ -22,7 +24,7 @@ public class UserControllerPublic {
     private final JwtProvider jwtProvider;
 
     @PostMapping("/login/kakao")
-    public ResponseEntity<CustomResponse<TokenResponse>> kakaoLogin(@RequestBody KakaoUserInfoDto req) {
+    public ResponseEntity<CustomResponse<TokenResponse>> kakaoLogin(@RequestBody KakaoLoginReq req) {
         Long userId = kakaoUserProcessor.processKakaoLogin(req.getId());
         TokenResponse tokenResponse = jwtProvider.generateTokens(userId, Role.USER);
         CustomResponse<TokenResponse> customResponse = new CustomResponse<>(tokenResponse);
