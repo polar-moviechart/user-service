688f0f22f4fb67b83d7fba18fffafd757422297e
stk346 <stk346@gmail.com>
2024-11-14 12:42:39 +0900

    refactor: UserControllerSecure.generateToken - 요청 형식 변경

--- src/main/java/com/polar_moviechart/userservice/domain/controller/secureapi/UserControllerSecure.java
+++ src/main/java/com/polar_moviechart/userservice/domain/controller/secureapi/UserControllerSecure.java
@@ -1,7 +1,9 @@
 package com.polar_moviechart.userservice.domain.controller.secureapi;
 
+import com.polar_moviechart.userservice.domain.entity.Role;
 import com.polar_moviechart.userservice.domain.service.jwt.JwtProvider;
 import com.polar_moviechart.userservice.utils.CustomResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,11 @@ public class UserControllerSecure {
     private final JwtProvider jwtProvider;
 
     @PostMapping("/generateToken")
-    public ResponseEntity<CustomResponse<AccessTokenDto>> generateToken(@RequestBody RefreshTokenDto req) {
-        String accessToken = jwtProvider.createAccessToken(req.getRefreshToken());
+    public ResponseEntity<CustomResponse<AccessTokenDto>> generateToken(
+            HttpServletRequest servletRequest) {
+        String userId = servletRequest.getHeader("X-User-Id");
+        String accessToken = jwtProvider.createAccessToken(Long.parseLong(userId), Role.USER);
+
         CustomResponse<AccessTokenDto> customResponse = new CustomResponse<>(new AccessTokenDto(accessToken));
         return ResponseEntity.ok(customResponse);
     }
