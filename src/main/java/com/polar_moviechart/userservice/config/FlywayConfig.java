f3cf486d7c5f5cac5210cf050ab463e6c2ad9834
stk346 <stk346@gmail.com>
2024-11-19 00:35:04 +0900

    feat: 테스트 환경에서 h2데이터베이스 사용 설정 및 깃허브 워크플로우 설정, flyway 설정

---
+++ src/main/java/com/polar_moviechart/userservice/config/FlywayConfig.java
@@ -0,0 +1,18 @@
+package com.polar_moviechart.userservice.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+//            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
+
