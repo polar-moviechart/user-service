203e2ad0ca04470ef88dcd436d98da152c684d6d
stk346 <stk346@gmail.com>
2024-11-19 06:23:17 +0900

    infra: WebConfig - 로컬호스트:포트 열어줌

--- src/main/java/com/polar_moviechart/userservice/config/WebConfig.java
+++ src/main/java/com/polar_moviechart/userservice/config/WebConfig.java
@@ -1,29 +1,17 @@
-//package com.polar_moviechart.userservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/v1/**")
-//                        .allowedOrigins("http://localhost:3000") // Next.js의 localhost 포트를 허용
-//                        .allowedMethods("GET", "POST", "PUT")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//
-//                registry.addMapping("/v3/api-docs")
-//                        .allowedOrigins("http://localhost:3000") // Next.js의 localhost 포트를 허용
-//                        .allowedMethods("GET")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-//}
+package com.polar_moviechart.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8082") // Swagger UI가 실행되는 URL을 명시
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
