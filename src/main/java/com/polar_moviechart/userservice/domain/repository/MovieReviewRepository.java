7b686a937cedb6f57f3a17fb504a00eeac13c6f0
stk346 <stk346@gmail.com>
2024-11-25 06:28:44 +0900

    feat: MovieReviewRepository 추가

---
+++ src/main/java/com/polar_moviechart/userservice/domain/repository/MovieReviewRepository.java
@@ -0,0 +1,13 @@
+package com.polar_moviechart.userservice.domain.repository;
+
+import com.polar_moviechart.userservice.domain.entity.MovieReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieReviewRepository extends JpaRepository<MovieReview, Long> {
+    Optional<List<MovieReview>> findByUserId(Long userId);
+}
