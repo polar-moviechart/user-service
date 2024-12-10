838f29162a67e4548d0deda73eeafe268e254b53
stk346 <stk346@gmail.com>
2024-12-11 05:05:00 +0900

    test: 구현 변경에 따라 테스트 코드 수정 및 삭제

--- src/test/java/com/polar_moviechart/userservice/domain/service/movie/MovieReviewCommandServiceTest.java
+++ src/test/java/com/polar_moviechart/userservice/domain/service/movie/MovieReviewCommandServiceTest.java
@@ -36,22 +36,4 @@ class MovieReviewCommandServiceTest extends MovieReviewTestConfig {
         // then
         assertNotNull(movieReviewRepository.findByUser_IdAndCode(getUserId(0), movieCode));
     }
-
-    @DisplayName("기존 리뷰가 있을 때 영화 리뷰를 업데이트 할 수 있다.")
-    @Test
-    void updateReviewTest_whenReviewExists() {
-        // given
-        int movieCode = 1;
-        initMovieReviews(1, List.of(movieCode), List.of(getUser(0)));
-        // when
-        String updatedContent = "updatedContent";
-        UpdateMovieReviewReq updateReq = UpdateMovieReviewReq.builder()
-                .content(updatedContent)
-                .build();
-        movieReviewCommandService.addReview(movieCode, getUser(0), updateReq);
-        // then
-        MovieReview updatedMovieReview = movieReviewRepository.findByUser_IdAndCode(getUserId(0), movieCode).get();
-        assertEquals(updatedContent, updatedMovieReview.getContent());
-    }
-
 }
