f6d21df7db05697f183d412bc4a17f6464694d33
stk346 <stk346@gmail.com>
2024-12-16 02:45:44 +0900

    refactor: 리뷰 추가했을 때 응답으로 닉네임, 내용, 수정일 추가

--- src/main/java/com/polar_moviechart/userservice/domain/service/movie/MovieReviewCommandService.java
+++ src/main/java/com/polar_moviechart/userservice/domain/service/movie/MovieReviewCommandService.java
@@ -22,6 +22,6 @@ public class MovieReviewCommandService {
                 .content(req.getContent())
                 .build();
         movieReviewRepository.save(movieReview);
-        return UpdateReviewRes.builder().reviewId(movieReview.getId()).build();
+        return UpdateReviewRes.from(movieReview);
     }
 }
